ac150deec7fb09d5f488f1d4b43656521672ebed
Linus XCJ <70716538+linusxcj@users.noreply.github.com>
2022-04-22 11:28:40 +0530

    update file

--- Name_meaning.py
+++ Name_meaning.py
@@ -30,21 +30,3 @@ except FileNotFoundError:
     print("The file 'names.txt' does not exist.")
     exit(1)
 
-for i in range(0,50):
-    # Select a random name from the list of names
-    random_name = random.choice(names)
-    rd = random.randint(1,30) #random date
-    rm = random.randint(2,8)  #random month
-
-    # Fetch the meaning of the random name
-    name_meaning = fetch_name_meaning(random_name)
-
-    print(f"The meaning of the name '{random_name}' is: {name_meaning}")
-
-    # Write the name and its meaning to "output.txt"
-    with open("output.txt", "w") as output_file:
-        output_file.write(f"Name: {random_name}, Meaning: {name_meaning}\n")
-        os.system("git pull origin master")
-        os.system("git add output.txt")
-        os.system(f'git commit --amend --date="2023-0{rm}-{rd}" --no-edit')
-        os.system('git push -u origin master')
