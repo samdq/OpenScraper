265ac578eea1a9b7218c8da32af855eb8f374670
Linus XCJ <70716538+linusxcj@users.noreply.github.com>
2022-04-25 11:29:20 +0530

    update file

--- Name_meaning.py
+++ Name_meaning.py
@@ -4,19 +4,3 @@ import random
 import os
 
 # Function to check if a string is alpha
-def is_alpha(s):
-    return s.isalpha()
-
-# Function to fetch the meaning of a name
-def fetch_name_meaning(name):
-    url = "https://www.thenamemeaning.com/"
-    url = url + name + "/"
-    r = requests.get(url)
-    data = r.content
-    soup = bs(data, "lxml")
-    try:
-        meaning = soup.strong.next_sibling.strip()
-        return meaning
-    except:
-        return "Not found"
-
