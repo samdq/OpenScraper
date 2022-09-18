7dafb2a266c922d5c3d9a7191f38d379f4b8c852
Linus XCJ <70716538+linusxcj@users.noreply.github.com>
2022-09-18 11:46:55 +0530

    update file

--- scraper.py
+++ scraper.py
@@ -2,3 +2,17 @@
 
 import requests
 from openscraper.selector_engine import SelectorEngine
+
+class Scraper:
+    def __init__(self, timeout=10):
+        self.timeout = timeout
+        self.selector_engine = SelectorEngine()
+
+    def fetch_html(self, url):
+        try:
+            response = requests.get(url, timeout=self.timeout)
+            response.raise_for_status()  # Raise an HTTPError for bad responses
+            return response.text
+        except requests.exceptions.RequestException as e:
+            print(f"Error fetching HTML from {url}: {e}")
+            return None
