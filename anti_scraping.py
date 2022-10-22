b29bdfca9424bb1bcd03c60595d41d5b326886af
Linus XCJ <70716538+linusxcj@users.noreply.github.com>
2022-10-22 11:50:03 +0530

    update file

--- anti_scraping.py
+++ anti_scraping.py
@@ -10,4 +10,6 @@ class AntiScrapingManager:
     def handle_cookies(self):
         print("Handling cookies...")
 
-
+    def handle_dynamic_content(self):
+        print("Waiting for dynamic content to load...")
+        time.sleep(randint(3, 7))  # Simulating a random delay between 3 to 7 seconds
