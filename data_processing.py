34075a609fe8d0234275b19b9936ba39c2a0fc75
Linus XCJ <70716538+linusxcj@users.noreply.github.com>
2022-10-25 11:50:31 +0530

    update file

---
+++ data_processing.py
@@ -0,0 +1,7 @@
+# openscraper/data_processing.py
+
+class DataProcessor:
+    def clean_data(self, raw_data):
+        # Placeholder for data cleaning logic
+        cleaned_data = [item.strip() for item in raw_data if item.strip()]
+        return cleaned_data
