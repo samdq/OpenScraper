3ffd6f83007da75e365c6486fba8a710ba55a200
Linus XCJ <70716538+linusxcj@users.noreply.github.com>
2023-01-23 10:17:52 +0530

    update file

--- api_integration.py
+++ api_integration.py
@@ -5,3 +5,12 @@ import requests
 class APIIntegration:
     def __init__(self, api_key=None):
         self.api_key = api_key
+    def integrate_with_external_api(self, api_url, data):
+        try:
+            headers = {"Authorization": f"Bearer {self.api_key}"} if self.api_key else {}
+            response = requests.post(api_url, json=data, headers=headers)
+            response.raise_for_status()
+            return response.json()
+        except requests.exceptions.RequestException as e:
+            print(f"Error integrating with external API {api_url}: {e}")
+            return None
